2cbe14510fcb5934a8df3d13726b3dc4cf5aecaa
Osamak063 <osamak063@gmail.com>
2021-04-13 16:51:36 +0500

    intial commit

---
+++ src/flappybird/Bird.java
@@ -0,0 +1,317 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flappybird;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Osama 3.1 gpa
+ */
+public class Bird {
+
+    EvolutionaryNeuralNetwork network;
+    Rectangle rectangle;
+    String image;
+    boolean isAlive;
+    int yMotion;
+    int fitness;
+
+    int rank;
+    double proportion;
+    double commulative;
+
+    //    only for the first time
+    void initializeNetwork() {
+
+        //creating initial nodes  
+        network.getInputLayer().add(new Node());
+        network.getInputLayer().add(new Node());
+
+        //setting hidden layer
+        network.getHiddenLayer().add(new Node());
+        network.getHiddenLayer().add(new Node());
+        network.getHiddenLayer().get(0).setBias(Mathematics.getRandom(-1.0, 0.5));
+        network.getHiddenLayer().get(1).setBias(Mathematics.getRandom(-1.0, 0.5));
+
+        //setting output  layer
+        network.getOutputLayer().add(new Node());
+        network.getOutputLayer().get(0).setBias(Mathematics.getRandom(-1.0, 0.5));
+
+        //initializing hidden layer nodes weights
+        network.initializeNodesWeights(network.getInputLayer(), network.getHiddenLayer());
+
+        //calculating output layer nodes wieghts
+        network.initializeNodesWeights(network.getHiddenLayer(), network.getOutputLayer());
+
+    }
+
+    public void jump(List<Rectangle> pipes) {
+
+        Rectangle pipe = null;
+//        horizontal distance from the start of the bird to the last edge of the pipe 
+//      first neural network input
+        double horizontalDistance = 3000;
+
+        /*  vertical distance from the bottom of the bird to the top of lower pipe
+           if difference is less thn make it 0
+        secondd neural network input*/
+        double verticalDistance;
+        double temp;
+
+//        checking the closest pipe
+        for (int i = 0; i < pipes.size(); i++) {
+
+//             check y!=0 so only lower pipes will be checked 
+            if (pipes.get(i).y != 0) {
+//            ending position of pipe - starting position of bird
+                temp = (pipes.get(i).x + pipes.get(i).width) - rectangle.x;
+
+//            temp should be greater thn zero so removed pipes wont be counted
+                if (temp >= 0 && temp < horizontalDistance) {
+
+                    horizontalDistance = temp;
+
+                    pipe = pipes.get(i);
+                }
+            }
+        }
+
+//        now we have the closest pipe in variable pipe
+//      calculate second input to neural network by calculating vertical difference
+        temp = pipe.y - (rectangle.y + rectangle.height);
+//        if difference is less thn 0 assign 0
+        verticalDistance = temp >= 0 ? temp : 0;
+
+//        dont know the maximu horizontal distance from bird to end of pipe so dividing by 700 analytically
+        horizontalDistance = horizontalDistance / 1000;
+//        minimum y position for the lower pipe will be 330 so normalizing vertical input value for ANN
+        verticalDistance = verticalDistance / 700;
+
+//        now feed forward neural netwokr with inputs, and return output
+        double networkOutput = network.feedForward(horizontalDistance, verticalDistance);
+
+//        if output> 0.5 jump otherwise not
+        if (networkOutput > 0.5) {
+
+//            jump
+            yMotion = 0;
+            yMotion -= 10;
+        }
+
+    }
+
+    public static List<Bird> sortFitness(List<Bird> array) {
+        int n = array.size();
+        for (int j = 1; j < n; j++) {
+            Bird key = array.get(j);
+            int i = j - 1;
+            while ((i > -1) && (Math.abs(array.get(i).getFitness()) < Math.abs(key.getFitness()))) {
+                array.set(i + 1, array.get(i));
+                i--;
+            }
+            array.set(i + 1, key);
+
+        }
+        return array;
+    }
+
+    public EvolutionaryNeuralNetwork getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(EvolutionaryNeuralNetwork network) {
+        this.network = network;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public void setRectangle(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+ 
+
+    public boolean isIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public int getyMotion() {
+        return yMotion;
+    }
+
+    public void setyMotion(int yMotion) {
+        this.yMotion = yMotion;
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public double getProportion() {
+        return proportion;
+    }
+
+    public void setProportion(double proportion) {
+        this.proportion = proportion;
+    }
+
+    public double getCommulative() {
+        return commulative;
+    }
+
+    public void setCommulative(double commulative) {
+        this.commulative = commulative;
+    }
+
+    @Override
+    public String toString() {
+        return "Bird{\n" + "network=" + network + "\n  , rank=" + rank + "\n Fitness " + fitness + '}';
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    
+}
+
+class EvolutionaryAlgorithm {
+
+    //this selection scheme is used only with population selection
+    //it will return best no of individuals in sorted order
+    public static ArrayList<Bird> truncationSelection(List<Bird> population, int noOfIndividualsRequired) {
+
+        //reverse sorting population on the basis of fitness
+        Bird.sortFitness(population);
+
+        return new ArrayList<Bird>(population.subList(0, noOfIndividualsRequired));
+    }
+
+    //only for parent selection
+    //it will return new array index 0  will contain 1st parent and 1 will contain 2nd parent
+    public static List<Bird> randomSelection(List<Bird> population, int noOfRequiredIndividuals) {
+
+        List<Bird> parents = new ArrayList<>();
+
+        int random1;
+        int random2;
+
+        while (parents.size() < noOfRequiredIndividuals) {
+            random1 = Mathematics.getRandom(0, population.size() - 1);
+            random2 = Mathematics.getRandom(0, population.size() - 1);
+
+//            System.out.println("ran1 " + random1 + " ran2 " + random2);
+            if (random1 != random2) {
+                parents.add(population.get(random1));
+                parents.add(population.get(random2));
+
+//                System.out.println("parent 1 " + parents.get(0).getFitness() + " " + parents.get(0));
+//                System.out.println("parent 2 " + parents.get(1).getFitness() + " " + parents.get(1));
+//                System.out.println(parents.size());
+            }
+        }
+        return parents;
+    }
+
+    public static Bird[] rankBased(Bird[] population, int noOfRequiredIndividuals) {
+
+        ArrayList<Bird> newRequiredPopulation = new ArrayList<>();
+        int i, ranksTotal = 0;
+        //sorting on the basis of fitness
+        Mathematics.sort(population);
+
+        //applying ranks
+        for (i = 0; i < population.length; i++) {
+            population[i].setRank(i + 1);
+            ranksTotal = ranksTotal + i + 1;
+        }
+//        System.out.println("total ranks " + ranksTotal);
+
+        //calculating rank proportion
+        for (i = 0; i < population.length; i++) {
+            population[i].setProportion(((double) population[i].getRank() / (double) ranksTotal));
+//            System.out.println("Proportion " + population[i].getProportion());
+        }
+
+        //commulating
+        //setting first individual commulative = to proportion
+        population[0].setCommulative(population[0].getProportion());
+        for (i = 1; i < population.length; i++) {
+            population[i].setCommulative(population[i - 1].getCommulative() + population[i].getProportion());
+//            System.out.println("Commulative " + population[i].getCommulative());
+
+        }
+
+        double random;
+        int j;
+        ///generating random number and selecting individuals for required population
+        while (newRequiredPopulation.size() < noOfRequiredIndividuals) {
+            random = Mathematics.getRandom();
+//            System.out.println("Random " + random);
+            //finding element in original population
+            for (j = 0; j < population.length - 1; j++) {
+
+                //if random number is less thn commulative of first element
+                if (random < population[0].getCommulative()) {
+//                    System.out.println("found elementtt " + random + " " + population[j].getCommulative() + " " + population[j]);
+                    break;
+                }
+                //if random number is equal to commulative of any element
+                if (population[j].getCommulative() == random) {
+//                    System.out.println("found element " + random + " " + population[j].getCommulative() + " " + population[j]);
+                    break;
+                }
+                //if random no fall between two numbers such that j < random >j+1 thn select j+1
+                if (population[j].getCommulative() < random
+                        && population[j + 1].getCommulative() >= random) {
+//                    System.out.println("found elementt " + population[j].getCommulative() + " " + random + " " + population[j + 1].getCommulative() + " " + population[j + 1]);
+                    j = j + 1;
+
+                    break;
+                }
+            }  // j will be the position of randomly selected element on the basis of commulative value
+
+            if (!newRequiredPopulation.contains(population[j])) {
+                newRequiredPopulation.add(population[j]);
+//                System.out.println("seleccted");
+            }
+
+        }
+
+//        for (int g = 0; g < newRequiredPopulation.size(); g++) {
+//            System.out.println("selected " + newRequiredPopulation.get(g).getCommulative());
+//        }
+        return newRequiredPopulation.toArray(new Bird[newRequiredPopulation.size()]);
+    }
+
+}
---
+++ src/flappybird/FlappyBird.java
@@ -0,0 +1,476 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flappybird;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+/**
+ *
+ * @author Osama 3.1 gpa
+ */
+public class FlappyBird implements ActionListener {
+
+    public static FlappyBird flappyBird;
+
+    public static final int N = 300;
+    public static final int M = 160;
+
+    public static int GENERATION = 0;
+
+    public final int WIDTH = 700, HEIGHT = 700;
+    int score;
+
+    boolean isScoreCounted;
+
+    int deadBirdsCount;
+
+    public final int TIMER_DELAY_IN_MILI_S = 20;
+    public final int PIPE_SPEED = 10;
+
+    Random rand;
+
+    static ArrayList<Bird> birds;
+    Rectangle bird;
+    Renderer renderer;
+    ArrayList<Rectangle> pipes;
+
+//    for bird motion
+    int ticks;
+    int yMotion = 0;
+
+    Timer timer;
+
+    boolean  isStarted;
+
+    FlappyBird() {
+
+//        initializing JFrame
+        JFrame frame = new JFrame();
+
+        timer = new Timer(TIMER_DELAY_IN_MILI_S, this);
+
+//        initializing Jpanel
+        renderer = new Renderer();
+
+//        setting JFrame attributes
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Flappy Bird");
+        frame.setVisible(true);
+
+        //     adding renderer to JFRAme
+        frame.add(renderer);
+
+//        setting all birds in the center of the screen x,y,width,height
+        for (Bird bird : birds) {
+            bird.setRectangle(new Rectangle(WIDTH / 2, HEIGHT / 2, 20, 20));
+
+        }
+
+        pipes = new ArrayList<>();
+
+        rand = new Random();
+
+        addPipe(true);
+        addPipe(true);
+        addPipe(true);
+        addPipe(true);
+
+//        starting timer
+        timer.start();
+
+    }
+
+    public void addPipe(boolean isStart) {
+
+//        vertical space bw pipes
+        int verticalSpace = 300;
+        int horizontalSpace = 600;
+        int width = 100;
+        int height = 50 + rand.nextInt(200);
+
+        if (isStart) {
+
+//            addding lower pipe ( horizontal space 600          , -120 for grass and land, ,                            
+            pipes.add(new Rectangle(WIDTH + width + pipes.size() * 300, HEIGHT - height - 120, width, height));
+//            upper pipe            x of prev lower pipe
+            pipes.add(new Rectangle(pipes.get(pipes.size() - 1).x, 0, width, HEIGHT - height - verticalSpace));
+        } else {
+//            lower pipe            (add 600 horizontal to prev pipe
+            pipes.add(new Rectangle(pipes.get(pipes.size() - 1).x + horizontalSpace, HEIGHT - height - 120, width, height));
+//            upper pipe
+            pipes.add(new Rectangle(pipes.get(pipes.size() - 1).x, 0, width, HEIGHT - height - verticalSpace));
+        }
+
+    }
+
+    public void paintPipe(Graphics graphics, Rectangle pipe) {
+
+        graphics.setColor(Color.green.darker());
+        graphics.fillRect(pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+
+    public void repaint(Graphics graphics) {
+
+//        setting full screen green screen color
+        graphics.setColor(Color.cyan);
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+
+
+        Image ground = Toolkit.getDefaultToolkit().getImage("assets/ground.png");
+        graphics.drawImage(ground,0 , HEIGHT-120, null);
+
+//        setting birds color
+        for (int i = 0; i < N; i++) {
+//            graphics.setColor(birds.get(i).getColor());
+//            graphics.fillRect(birds.get(i).getRectangle().x, birds.get(i).getRectangle().y,
+//                    birds.get(i).getRectangle().width, birds.get(i).getRectangle().height);
+
+            Image img = Toolkit.getDefaultToolkit().getImage(birds.get(i).getImage());
+            graphics.drawImage(img, (birds.get(i).getRectangle().x) - 9, birds.get(i).getRectangle().y - 2, null);
+        }
+
+//        painting pipes
+        for (Rectangle pipe : pipes) {
+            paintPipe(graphics, pipe);
+        }
+
+//        showing game over or click to start
+        graphics.setColor(Color.WHITE);
+        graphics.setFont(new Font("Arial", 1, 80));
+
+        if (!isStarted) {
+            graphics.drawString("Click to start!", 75, HEIGHT / 2 - 50);
+        } else {
+            graphics.drawString(String.valueOf(GENERATION), WIDTH / 2 - 25, 100);
+        }
+
+//        setting birds alive text
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(new Font("Arial", 1, 30));
+        graphics.drawString("Birds Alive", 50, 50);
+
+//        setting birds alive count
+        graphics.setColor(Color.WHITE);
+        graphics.setFont(new Font("Arial", 1, 30));
+        graphics.drawString(String.valueOf(N - deadBirdsCount), 90, 90);
+
+//        showing score text
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(new Font("Arial", 1, 30));
+        graphics.drawString("Score", 550, 50);
+
+        //        showing score 
+        graphics.setColor(Color.WHITE);
+        graphics.setFont(new Font("Arial", 1, 30));
+        graphics.drawString(String.valueOf(score), 575, 90);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+//        timer call back, this will execute after every 20 mili seconds
+        ticks++;
+
+        if (isStarted) {
+
+//        moving rectangles
+            for (Rectangle pipe : pipes) {
+                pipe.x -= PIPE_SPEED;
+            }
+
+//            checking is bird needs to jump
+            for (int i = 0; i < N; i++) {
+                if (birds.get(i).isAlive) {
+//                        this method will decide if jump is needed
+                    birds.get(i).jump(pipes);
+
+//                    setting fitness
+                    birds.get(i).setFitness(ticks);
+                }
+            }
+
+//            calculating bird fall  motion
+            for (int i = 0; i < N; i++) {
+                if (birds.get(i).isAlive && ticks % 2 == 0 && birds.get(i).getyMotion() < 15) {
+//                    yMotion += 2;
+                    birds.get(i).setyMotion(birds.get(i).getyMotion() + 2);
+
+                }
+
+//                this will run eveytime for every bird alive
+                if (birds.get(i).isAlive) {
+
+//                    bird fall motion
+//                    bird.y += yMotion;
+                    birds.get(i).getRectangle().y += birds.get(i).getyMotion();
+                }
+            }
+
+//        removing pipes that are gone from the screen
+//        it was crashing with for each loop thats why using thi loop
+            for (int i = 0; i < pipes.size(); i++) {
+
+                Rectangle pipe = pipes.get(i);
+
+//            if pipe is gone
+                if (pipe.x + pipe.getWidth() <= 0) {
+
+//                because we added lower pipe first so lower pipe will be removed and thn upper
+                    pipes.remove(pipe);
+                    /*   because we have to remove two pipes(upper and lower) simultaneously we will add new pipe
+                     only when both(upper and lower) pipes are removed*/
+
+//                upper pipe will have y coordinate==0
+                    if (pipe.y == 0) {
+                        addPipe(false);
+                    }
+                }
+            }
+
+//        checking game terminating condtiiton and collisions
+            for (Rectangle pipe : pipes) {
+
+//                checking collision of  pipes with all birds
+                for (int i = 0; i < N; i++) {
+
+                    if (birds.get(i).isAlive) {
+//                calculating score checking for y=0 so only one time score is count not for two times for upper and lower pipes
+                        if (!isScoreCounted && (pipe.y == 0 && pipe.x + pipe.width == birds.get(i).getRectangle().x)) {
+                            score++;
+                            isScoreCounted = true;
+
+                        }
+
+//                        checking collision
+                        if (pipe.intersects(birds.get(i).getRectangle())) {
+//                            gameOver = true;
+                            birds.get(i).setIsAlive(false);
+                            deadBirdsCount++;
+
+//                            setting bird to bottom of the screen
+                            birds.get(i).getRectangle().y = HEIGHT;
+
+                        }
+
+//                        checking collision
+                        if (birds.get(i).isAlive && (birds.get(i).getRectangle().y > HEIGHT - 120 || birds.get(i).getRectangle().y <= 0)) {
+//                            gameOver = true;
+                            birds.get(i).setIsAlive(false);
+                            deadBirdsCount++;
+
+//                           setting bird to bottom of the screen
+                            birds.get(i).getRectangle().y = HEIGHT  ;
+
+                        }
+                    }
+                }
+            }
+
+            isScoreCounted = false;
+
+//               calling repaing of renderer will call paint component in Renderer which eventually will call repaint of Flappy bird(this class)
+            renderer.repaint();
+
+            
+            if (deadBirdsCount == N) {
+                isStarted = false;
+
+//                start next generation
+                performEvolution();
+            }
+        }
+
+    }
+ public static void playMusicInBackground() {
+
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+        executorService.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+
+                try {
+                    FileInputStream fileInputStream = new FileInputStream("music.mp3");
+                    Player player = new Player(fileInputStream);
+                    player.play();
+
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (JavaLayerException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        }, 0, 141, TimeUnit.SECONDS);
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        playMusicInBackground();
+
+        Color colors[] = {Color.GRAY, Color.BLUE, Color.YELLOW, Color.RED, Color.MAGENTA, Color.ORANGE, Color.black, Color.PINK, Color.darkGray, Color.LIGHT_GRAY,};
+        String[] images = {"assets/flappy-bird.png", "assets/flappy-bird -brown.png", "assets/flappy-bird -green.png", "assets/flappy-bird -pink.png", "assets/flappy-bird -red.png"};
+
+        birds = new ArrayList<>();
+
+//        for images
+        int j = 0;
+        //initializing population
+        for (int i = 0; i < N; i++) {
+
+//            creating bird
+            Bird bird = new Bird();
+//            creating network
+            bird.setNetwork(new EvolutionaryNeuralNetwork());
+//            setting bird color
+//            bird.setColor(colors[i]);
+            bird.setImage(images[j]);
+
+            bird.setIsAlive(true);
+
+            birds.add(bird);
+            //randomly initializing networks
+            bird.initializeNetwork();
+
+            //setting initial fitness to 0
+            bird.setFitness(0);
+
+            j++;
+            if (j == 5) {
+                j = 0;
+            }
+        }
+
+//      starting game
+        flappyBird = new FlappyBird();
+        flappyBird.isStarted = true;
+    }
+
+    public void performEvolution() {
+
+        GENERATION++;
+
+        List<Bird> children = new ArrayList<>();
+
+        for (int i = 0; i < M; i = i + 2) {
+
+            //STEP #2.1 different parents
+            Bird[] birdsArray = birds.toArray(new Bird[birds.size()]);
+
+//            List<Bird> parent = EvolutionaryAlgorithm.rankBased(birdsArray, 2);
+            List<Bird> parent = Arrays.asList(EvolutionaryAlgorithm.rankBased(birds.toArray(new Bird[birds.size()]), 2));
+            System.out.println("parentttttttttttttt 1 " + parent.get(0).toString());
+            System.out.println("parentttttttttttttt 2 " + parent.get(1).toString());
+            //STEP #2.2 apply crossovers and produce 2 children
+            Bird child1 = new Bird();
+            Bird child2 = new Bird();
+
+            child1.setNetwork(new EvolutionaryNeuralNetwork());
+            child2.setNetwork(new EvolutionaryNeuralNetwork());
+            //true for first child , false for second child
+
+            child1.getNetwork().crossOver(parent, true);
+            child2.getNetwork().crossOver(parent, false);
+
+            child1.setImage(parent.get(1).getImage());
+            child2.setImage(parent.get(0).getImage());
+
+//            adding children
+            children.add(child1);
+            children.add(child2);
+
+            //STEP #2.3 APPLY MUTATION ON TWO PRODUCED CHILDREN OF 0.25
+//                   System.out.println("before "+children.get(children.size()-2).toString());
+            children.get(children.size() - 2).getNetwork().mutate();
+            children.get(children.size() - 1).getNetwork().mutate();
+//                   System.out.println("afer "+children.get(children.size()-2).toString());
+
+            //STEP # 2.4 SETTING FITNESS AS AVERAGE OF TWO PARENTS
+            children.get(children.size() - 2).setFitness(((parent.get(0).getFitness() + parent.get(1).getFitness()) / 2) - 6 + new Random().nextInt(20));
+            children.get(children.size() - 1).setFitness(((parent.get(0).getFitness() + parent.get(1).getFitness()) / 2) - 6 + new Random().nextInt(20));
+
+//            System.out.println("CHILLDDDDDDDD 1 "+ child1.toString());
+//            System.out.println("CHILDDDDDDDDD 2 "+child2.toString());
+        }
+
+        //STEP # 3 FROM m+n individuals select n fittest individuals
+        birds.addAll(children);
+        //population selection and resetting population
+        birds = EvolutionaryAlgorithm.truncationSelection(birds, N);
+
+        System.out.println("GENERATION " + GENERATION + " Best Fitness " + birds.get(0).getFitness());
+
+        for (int i = 0; i < birds.size(); i++) {
+            System.out.println(birds.get(i).toString());
+            System.out.println("Fitness " + birds.get(i).getFitness());
+        }
+        //    start game again
+        resetGame();
+        flappyBird.isStarted = true;
+    }
+
+//    reset game for new generation
+    public void resetGame() {
+
+        // setting all birds in the center of the screen x,y,width,height
+        for (Bird bird : birds) {
+            bird.setRectangle(new Rectangle(WIDTH / 2, HEIGHT / 2, 20, 20));
+
+//            setting y motion to 0
+            bird.setyMotion(0);
+            bird.setIsAlive(true);
+            bird.setFitness(0);
+        }
+
+        deadBirdsCount = 0;
+        ticks = 0;
+        score = 0;
+
+        pipes = new ArrayList<>();
+
+        rand = new Random();
+
+        addPipe(true);
+        addPipe(true);
+        addPipe(true);
+        addPipe(true);
+
+    }
+
+}
